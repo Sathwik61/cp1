2be54c38811fc82a535945c90aa79848aba161e8
Saurabh <61317144+0xSaurabhx@users.noreply.github.com>
2024-05-01 19:35:14 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,18 @@
-import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from './Login'
-import SignupPage from './SignupPage'
-import Home from './Home'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import SignupPage from "./SignupPage";
+import Home from "./Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<Login />} />
-    <Route path="/signup" element={<SignupPage />} />
-    <Route path="/dashboard" element={<Home />} />
-      </Routes>     
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </BrowserRouter>
-       
-      
-    
-  )
+  );
 }
 
-export default App
+export default App;
